66e7fff02cd2425ffcc55aa644013fb980c101a6
Rafael Simplicio <simplicio.rsb@gmail.com>
2023-10-04 11:10:16 -0300

    feat: use case get user profile

---
+++ src/use-cases/get-user-profile.spec.ts
@@ -0,0 +1,38 @@
+import { expect, describe, it, beforeEach } from 'vitest'
+import { InMemoryUsersRepository } from '@/repositories/in-memory/in-memory-users-repository'
+import { GetUserProfileUseCase } from './get-user-profile'
+import { hash } from 'bcryptjs'
+import { ResourcesNotFoundError } from './errors/resources-not-found-erro'
+
+let usersRepository: InMemoryUsersRepository
+let sut: GetUserProfileUseCase
+
+describe('Get User Profile Use Case', () => {
+  beforeEach(() => {
+    usersRepository = new InMemoryUsersRepository()
+    sut = new GetUserProfileUseCase(usersRepository)
+  })
+
+  it('should be able to get user profile', async () => {
+    const createdUser = await usersRepository.create({
+      name: 'John Doe',
+      email: 'john.doe@gmail.com',
+      password_hash: await hash('123456', 6),
+    })
+
+    const { user } = await sut.execute({
+      userId: createdUser.id,
+    })
+
+    expect(user.id).toEqual(expect.any(String))
+    expect(user.name).toEqual('John Doe')
+  })
+
+  it('should not be able to get user profile with wrong id', async () => {
+    expect(() =>
+      sut.execute({
+        userId: 'non-existing-id',
+      }),
+    ).rejects.toBeInstanceOf(ResourcesNotFoundError)
+  })
+})
