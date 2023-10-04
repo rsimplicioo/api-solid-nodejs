68bc3cdaeafc6c30b657d10a40794b26f92aa5d0
Rafael Simplicio <simplicio.rsb@gmail.com>
2023-10-04 19:15:29 -0300

    feat: check-in use case

--- src/use-cases/authenticate.spec.ts
+++ src/use-cases/authenticate.spec.ts
@@ -29,7 +29,7 @@ describe('Authenticate Use Case', () => {
   })
 
   it('should be able to authenticate with wrong email', async () => {
-    expect(() =>
+    await expect(() =>
       sut.execute({
         email: 'john.doe@gmail.com',
         password: '123456',
@@ -44,7 +44,7 @@ describe('Authenticate Use Case', () => {
       password_hash: await hash('123456', 6),
     })
 
-    expect(() =>
+    await expect(() =>
       sut.execute({
         email: 'john.doe@gmail.com',
         password: '654321',
